4aa3e19d45bf7a33859aceb48bfb37021d2e4683
marooncoucal <marooncoucal@gmail.com>
2025-06-26 09:52:17 +0300

    cardData editing

--- src/app/components/content/cardDataVault.js
+++ src/app/components/content/cardDataVault.js
@@ -1,11 +1,11 @@
 export const cardData1 = [
     {
         id: 1,
-        link: 'https://www.awwwards.com/',
-        image: '/img/awards.png',
-        title: 'Awwwards',
-        description: 'Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе',
-        tags: ['веб-дизайн', 'сайт', "для вдохновения", "для работы"]
+        link: 'https://www.wiley.com/en-gb/About+Face%3A+The+Essentials+of+Interaction+Design%2C+4th+Edition-p-9781118766583',
+        image: '/img/aboutface.png',
+        title: 'Об интерфейсах: основы интерактивного дизайна',
+        description: 'Книга про интерактивный дизайн, продолжает лидировать в идеях и методах для практиков дизайна и разработчиков.',
+        tags: ["для обучения", 'текст', 'книга', 'веб-дизайн']
     },
     {
         id: 2,
@@ -25,51 +25,12 @@ export const cardData1 = [
     },
     {
         id: 4,
-        link: 'https://www.wiley.com/en-gb/About+Face%3A+The+Essentials+of+Interaction+Design%2C+4th+Edition-p-9781118766583',
-        image: '/img/aboutface.png',
-        title: 'Об интерфейсах: основы интерактивного дизайна',
-        description: 'Книга про интерактивный дизайн, продолжает лидировать в идеях и методах для практиков дизайна и разработчиков.',
-        tags: ["для обучения", 'текст', 'книга', 'веб-дизайн']
-    },
-    {
-        id: 5,
-        link: 'https://deadsign.ru/workflow/webs_lost_soul/',
-        image: '/img/space.png',
-        title: 'Куда пропала душа веба?',
-        description: 'Статья про тренды в вебе и поиск причин одинаковости сайтов',
-        tags: ["для обучения", 'текст', 'статья', 'веб-дизайн']
-    },
-    {
-        id: 6,
         link: 'https://www.awwwards.com/',
         image: '/img/awards.png',
         title: 'Awwwards',
         description: 'Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе',
-        tags: ['веб-дизайн', 'сайт', "для вдохновения"]
-    },
-    {
-        id: 7,
-        link: 'https://cantunsee.space/',
-        image: '/img/unsee.png',
-        title: `Can't Unsee`,
-        description: 'Игра для дизайнеров для проверки знаний в дизайне интерфейсов',
-        tags: ['веб-дизайн', 'игра', "для вдохновения"]
-    },
-    {
-        id: 8,
-        link: 'https://hydra.ojack.xyz/',
-        image: '/img/hydra.png',
-        title: 'Hydra: Live Coding Visuals',
-        description: 'Среда кодирования с возможностью изменения кода в реальном времени, которая запускается непосредственно в браузере.',
-        tags: ['код', 'анимация', 'синтезатор', 'видео', "для вдохновения", "для работы"]
-    },
-    {
-        id: 9,
-        link: 'https://www.wiley.com/en-gb/About+Face%3A+The+Essentials+of+Interaction+Design%2C+4th+Edition-p-9781118766583',
-        image: '/img/aboutface.png',
-        title: 'Об интерфейсах: основы интерактивного дизайна',
-        description: 'Книга про интерактивный дизайн, продолжает лидировать в идеях и методах для практиков дизайна и разработчиков.',
-        tags: ['обучение', 'текст', 'книга', 'веб-дизайн', "для вдохновения", "для работы"]
+        tags: ['веб-дизайн', 'сайт', "для вдохновения", "для работы"]
+
     },
 ]
 
@@ -77,42 +38,34 @@ export const cardData1 = [
 export const cardData2 = [
     {
         id: 1,
-        link: 'https://www.awwwards.com/',
-        image: '/img/awards.png',
-        title: 'Awwwards',
-        description: 'Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе',
-        tags: ['веб-дизайн', 'сайт', "для вдохновения", "для работы"]
+        link: 'https://www.mann-ivanov-ferber.ru/catalog/product/gettingtoyes/?srsltid=AfmBOopG2LnjgItDPrzeZneCfOOKRNQux0rGef2fhejR-MdMX007ugBJ',
+        image: '/img/alenaKukushkina/NegotiationHarvardFisher.jpg',
+        title: `Переговоры без поражения. Гарвардский метод`,
+        description: `Авторы предлагают метод принципиальных переговоров, основанный на поиске взаимовыгодных решений, а не на конфронтации. Книга сочетает теоретические основы с практическими примерами.`,
+        tags: ["для обучения", "для работы", `литература`, `авторы`]
     },
     {
         id: 2,
-        link: 'https://cantunsee.space/',
-        image: '/img/unsee.png',
-        title: `Can't Unsee`,
-        description: 'Игра для дизайнеров для проверки знаний в дизайне интерфейсов',
-        tags: ['веб-дизайн', 'игра', "для работы"]
+        link: 'https://www.mann-ivanov-ferber.ru/catalog/product/emocii_v_peregovorah/?srsltid=AfmBOoqPIun3qjOSRsMoL8Nelzltc5v1TE1ZXcvH-p-kImKplzDFf3ZD',
+        image: '/img/alenaKukushkina/EmotionsFisher.jpg',
+        title: `Переговоры с удовольствием. Как превратить эмоции в союзников`,
+        description: 'Книга посвящена роли эмоций в переговорах. Авторы объясняют, как управлять своими эмоциями и влиять на эмоции оппонента, чтобы достичь лучших результатов.',
+        tags: ["для обучения", "для работы", `литература`, `авторы`]
     },
     {
         id: 3,
-        link: 'https://hydra.ojack.xyz/',
-        image: '/img/hydra.png',
-        title: 'Hydra: Live Coding Visuals',
-        description: 'Среда кодирования с возможностью изменения кода в реальном времени, которая запускается непосредственно в браузере.',
-        tags: ['код', 'анимация', 'синтезатор', 'видео', "для работы"]
+        link: 'https://www.ozon.ru/search/?deny_category_prediction=true&from_global=true&text=%D0%93%D0%B5%D0%BD%D0%B8%D0%B9+%D0%BF%D0%B5%D1%80%D0%B5%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%BE%D0%B2.+%D0%9A%D0%B0%D0%BA+%D0%BF%D1%80%D0%B5%D0%BE%D0%B4%D0%BE%D0%BB%D0%B5%D1%82%D1%8C+%D0%BF%D1%80%D0%B5%D0%BF%D1%8F%D1%82%D1%81%D1%82%D0%B2%D0%B8%D1%8F+%D0%B8+%D0%B4%D0%BE%D1%81%D1%82%D0%B8%D1%87%D1%8C+%D0%B1%D0%BB%D0%B5%D1%81%D1%82%D1%8F%D1%89%D0%B8%D1%85+%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D0%BE%D0%B2+%D0%B7%D0%B0+%D1%81%D1%82%D0%BE%D0%BB%D0%BE%D0%BC+%D0%BF%D0%B5%D1%80%D0%B5%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%BE%D0%B2+%D0%B8+%D0%B7%D0%B0+%D0%B5%D0%B3%D0%BE+%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B0%D0%BC%D0%B8+%7C+%D0%9C%D0%B0%D0%BB%D1%85%D0%BE%D1%82%D1%80%D0%B0+%D0%94%D0%B8%D0%BF%D0%B0%D0%BA%2C+%D0%91%D0%B0%D0%B7%D0%B5%D1%80%D0%BC%D0%B0%D0%BD+%D0%9C%D0%B0%D0%BA%D1%81+%D0%A5.&product_id=231026195',
+        image: '/img/alenaKukushkina/MalhotraBazerman.jpg',
+        title: `Переговоры с удовольствием. Как превратить эмоции в союзников`,
+        description: `Авторы, профессора Гарвардской школы бизнеса, предлагают научно обоснованные стратегии для сложных переговоров. Книга включает кейсы из бизнеса, политики и повседневной жизни.`,
+        tags: ["для обучения", "для работы", `литература`, `авторы`]
     },
     {
         id: 4,
-        link: 'https://www.wiley.com/en-gb/About+Face%3A+The+Essentials+of+Interaction+Design%2C+4th+Edition-p-9781118766583',
-        image: '/img/aboutface.png',
-        title: 'Об интерфейсах: основы интерактивного дизайна',
-        description: 'Книга про интерактивный дизайн, продолжает лидировать в идеях и методах для практиков дизайна и разработчиков.',
-        tags: ["для обучения", 'текст', 'книга', 'веб-дизайн']
-    },
-    {
-        id: 5,
-        link: 'https://deadsign.ru/workflow/webs_lost_soul/',
-        image: '/img/space.png',
-        title: 'Куда пропала душа веба?',
-        description: 'Статья про тренды в вебе и поиск причин одинаковости сайтов',
-        tags: ["для обучения", 'текст', 'статья', 'веб-дизайн']
+        link: 'https://www.litres.ru/book/raznoe/peregovory-56306355/chitat-onlayn/',
+        image: '/img/alenaKukushkina/HowardNegotiation.jpg',
+        title: `Переговоры с удовольствием. Как превратить эмоции в союзников`,
+        description: `Одна из первых книг, которая систематизировала переговоры как науку. Райффа, профессор Гарварда, рассматривает переговоры с точки зрения теории игр и математических моделей.`,
+        tags: ["для обучения", "для работы", `литература`, `авторы`]
     },
 ]
